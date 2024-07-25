bbf8bfd7c99aa1d038430b48a965e520dfe5445f
Dinesh Kumar <dineshkumar90n@gmail.com>
2024-07-25 16:17:57 +0530

    tech: add index on book title

--- backend/src/main/java/com/bnpp/bookstore/entities/Book.java
+++ backend/src/main/java/com/bnpp/bookstore/entities/Book.java
@@ -1,7 +1,6 @@
 package com.bnpp.bookstore.entities;
 
 import jakarta.persistence.*;
-import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
